66add5e64c6a72ab8acb0490b0f68e8dd9c67e2e
Maribelens <maribelensosa@gmail.com>
2025-10-27 17:33:18 -0300

    reorganización de archivos del proyecto

---
+++ Assets/Scripts/Ui/PauseUI.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] private GameObject panelMainPause;
+    //[SerializeField] private GameObject panelCredits;
+    [SerializeField] private GameObject panelSettings;
+
+    [Header("Buttons")]
+    [SerializeField] private Button btnPlay;
+    [SerializeField] private Button btnOptions;
+    [SerializeField] private Button btnExit;
+    //[SerializeField] private Button btnCreditsBack;
+    [SerializeField] private Button btnSettingsBack;
+
+    private bool isPause;
+
+    private void Awake()
+    {
+        btnPlay.onClick.AddListener(OnPlayClicked);
+        btnOptions.onClick.AddListener(OnOptionsClicked);
+        btnExit.onClick.AddListener(OnExitClicked);
+        btnSettingsBack.onClick.AddListener(OnSettingsBackClicked);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        isPause = !isPause;
+        panelMainPause.SetActive(isPause);
+        if (isPause)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+
+    public void OnPlayClicked()
+    {
+        TogglePause();
+    }
+
+    private void OnOptionsClicked()
+    {
+        panelSettings.SetActive(true);
+    }
+    private void OnExitClicked()
+    {
+        ExitGame();
+    }
+
+    private void ExitGame()
+    {
+        //#if UNITY_EDITOR
+        //        UnityEditor.EditorApplication.isPlaying = false;
+        //#endif
+        SceneManager.LoadScene(0);
+    }
+
+    private void OnSettingsBackClicked()
+    {
+        panelSettings.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        btnPlay.onClick.RemoveAllListeners();
+        btnOptions.onClick.RemoveAllListeners();
+        btnExit.onClick.RemoveAllListeners();
+        btnSettingsBack.onClick.RemoveAllListeners();
+    }
+}
